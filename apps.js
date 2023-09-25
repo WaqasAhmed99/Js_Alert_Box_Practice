6db6ef59db72699a10e2344637622950ff38c48f
Waqas Ahmed <137869352+WaqasAhmed99@users.noreply.github.com>
2023-09-25 01:18:11 -0700

    Add files via upload
    SMIT JavaScript Alert Box 1st Assignment

---
+++ apps.js
@@ -0,0 +1,5 @@
+alert("Error! Please enter a valid password.")
+alert("Welcome to JS Land... \n Happy Coding!")
+alert("Welcome to JS Land...")
+alert("Happy Coding!")
+console.log("Hello... I can run JS throgh my web browser's console");
